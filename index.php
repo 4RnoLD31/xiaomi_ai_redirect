3779e4ed59c8ef1048bcb1588615054ef8716c43
4RnoLD31 <116956555+4RnoLD31@users.noreply.github.com>
2022-10-30 20:28:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
+<?php
+ $content = array (
+  2717501C_1000 => {"code":10000,"status":0,"result":[]}
+ );
+ $modelClientId = $_GET('modelClientId')
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
 <html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Заголовок страницы для браузера</title>
-  
- </head>
- <body>
- </body>
+  <head>
+   <meta charset="utf-8">
+   <title>Моя тестовая страница</title>
+  </head>
+  <body>
+   <?php
+    foreach ($content as $key => $value)
+    {
+     echo "<a href=\ "index.php?modelClientId=$key\">$value</a>";
+    }
+   ?>
+  </body>
 </html>
